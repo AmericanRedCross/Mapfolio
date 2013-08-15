60fa3b9d8fe8607885d26d0c6ec6a3b9c75ceee9
Daniel Joseph <djosep@gmail.com>
2013-08-15 10:44:25 -0400

    disable mouse zoom in map (issue resolved)
    + starting to work on marker cluster

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+var geojson;
 var center = new L.LatLng(30, 30);
 var bounds = new L.LatLngBounds([90, 200], [-80, -200]);
 worldcountries = [];
+centroids = [];
+
+var markers = new L.MarkerClusterGroup();
 
 var greyStyle = {
     color: '#fff',
@@ -13,10 +17,19 @@ var map = L.map('map', {
     center: center,
     zoom: 0,
     attributionControl: false,
-    zoomControl: false,
-    maxBounds: bounds
+    maxBounds: bounds,
+    scrollWheelZoom: false,
     // dragging: false
 });
+
+var centroidOptions = {
+    radius: 8,
+    fillColor: "#ED1B2E",
+    color: "#FFF",
+    weight: 2.5,
+    opacity: 1,
+    fillOpacity: 1
+};
 
 function getWorld() {
     $.ajax({
@@ -27,16 +40,55 @@ function getWorld() {
         timeout: 10000,
         success: function(json) {
             worldcountries = json;
-            worldcountriesLayer = L.geoJson(worldcountries, {
-        		style: greyStyle,        		
-    		});
-    		worldcountriesLayer.addTo(map);
+            countries = new L.layerGroup().addTo(map);
+            geojson = L.geoJson(worldcountries,{
+                style: greyStyle
+            }).addTo(countries);
+
         },
         error: function(e) {
             console.log(e);
         }
     });
 }
+
+function getCentroids(){
+    $.ajax({
+        type: 'GET',
+        url: 'data/centroids.json',
+        contentType: 'application/json',
+        dataType: 'json',
+        timeout: 10000,
+        success: function(json) {
+            centroids = json;
+            markers2map();
+        },
+        error: function(e) {
+            console.log(e);
+        }
+    });
+}
+
+function markers2map () {
+    markers = new L.MarkerClusterGroup();
+    marker = L.geoJson(centroids, {
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, centroidOptions);
+        },
+     // onEachFeature: markerEvents
+    });
+    markers.addLayer(marker);
+    map.addLayer(markers);
+
+    // centroidsLayer = L.geoJson(centroids,{
+    //  style: centroidOptions
+    // });
+    // centroidsLayer.addTo(map);       
+
+
+}
+
+
 
 function toggleSector (sectorClass, element) {
 	var status = $(element).children();
